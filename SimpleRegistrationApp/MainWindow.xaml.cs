0d108241cc9af8c44bcccb3bff77eea977562bd7
timurqqe <timurkkq@gmail.com>
2025-11-21 20:14:39 +0300

    Add files via upload

---
+++ SimpleRegistrationApp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleRegistrationApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(name.Text))
+            {
+                MessageBox.Show("Введите имя", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (!int.TryParse(age.Text, out int x) || x < 1 || x > 120)
+            {
+                MessageBox.Show("Введите корректный возраст (1-120)", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (password.Password.Length < 6)
+            {
+                MessageBox.Show("Пароль должен содежать минимум 6 символов", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            MessageBox.Show("Регистрация успешна!", "Информаиця", MessageBoxButton.OK, MessageBoxImage.Information);
+            
+        }
+    }
+}
